0864d683c1957e774df23caa23e5b48568b1e408
Animesh478 <animeshmohanty.am@gmail.com>
2024-05-27 23:16:37 +0530

    improved the image of the hero section and modified the customer details section

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-const target = document.querySelector(".event-demo h3");
-const modal = document.querySelector(".modal");
-target.addEventListener("mouseover", () => {
-  modal.classList.toggle("hidden");
-});
+
